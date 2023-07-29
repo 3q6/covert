4e8205665c589e1382d10640965bcdd33049356e
Cube <73487410+3q6@users.noreply.github.com>
2023-07-29 18:42:13 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="dumb dumb didily dumb" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          your mum
         </p>
       </main>
 
